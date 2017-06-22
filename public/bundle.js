3a5186269db3031a98b03aaab2bdf00897deeddf
KIM SUNGWON <ksw7521@gmail.com>
2017-06-22 15:39:31 +0900

    Add grid system & style examples

--- public/bundle.js
+++ public/bundle.js
@@ -24932,11 +24932,14 @@
 	    null,
 	    React.createElement(Nav, null),
 	    React.createElement(
-	      'h2',
-	      null,
-	      'Main Component'
-	    ),
-	    props.children
+	      'div',
+	      { className: 'row' },
+	      React.createElement(
+	        'div',
+	        { className: 'columns medium-6 large-4 small-centered' },
+	        props.children
+	      )
+	    )
 	  );
 	};
 
@@ -26819,6 +26822,9 @@
 
 	var React = __webpack_require__(8);
 
+	var _require = __webpack_require__(166),
+	    Link = _require.Link;
+
 	// var Examples = React.createClass({
 	//   render: function() {
 	//     return (
@@ -26833,13 +26839,35 @@
 	    null,
 	    React.createElement(
 	      'h3',
-	      null,
+	      { className: 'text-center' },
 	      'Examples'
 	    ),
 	    React.createElement(
 	      'p',
 	      null,
-	      'Welcome to example page!'
+	      'Here are a few example locations to try out'
+	    ),
+	    React.createElement(
+	      'ol',
+	      null,
+	      React.createElement(
+	        'li',
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: '/?location=Philadelphia' },
+	          'Philadelphia, PA'
+	        )
+	      ),
+	      React.createElement(
+	        'li',
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: '/?location=Rio' },
+	          'Rio, Brazil'
+	        )
+	      )
 	    )
 	  );
 	};
